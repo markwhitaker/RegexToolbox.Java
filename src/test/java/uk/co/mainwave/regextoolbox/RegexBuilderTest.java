106fcc0d98ef38f79565cd3947d9eb0a4ac389bd
Mark Whitaker <mark@mainwave.co.uk>
2018-09-10 11:03:01 +0100

    Broke another test

--- src/test/java/uk/co/mainwave/regextoolbox/RegexBuilderTest.java
+++ src/test/java/uk/co/mainwave/regextoolbox/RegexBuilderTest.java
@@ -38,7 +38,7 @@ public class RegexBuilderTest {
         Assert.assertFalse(regex.matcher(Strings.SimpleHttpUrl).find());
         Assert.assertFalse(regex.matcher(Strings.SimpleHttpsUrl).find());
         Assert.assertFalse(regex.matcher(Strings.Ipv4Address).find());
-        Assert.assertFalse(regex.matcher(Strings.Ipv6Address).find());
+        Assert.assertTrue(regex.matcher(Strings.Ipv6Address).find());
         Assert.assertTrue(regex.matcher(Strings.MacAddress).find());
     }
 
