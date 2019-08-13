2707142edf02fb070bd4a1a1aa0e64f0a4f1ce96
Mark Whitaker <mark@mainwave.co.uk>
2019-08-13 12:07:09 +0100

    Switched from Maven to Gradle

--- src/main/java/uk/co/mainwave/regextoolbox/RegexBuilderException.java
+++ src/main/java/uk/co/mainwave/regextoolbox/RegexBuilderException.java
@@ -3,7 +3,6 @@ package uk.co.mainwave.regextoolbox;
 /**
  * Exception thrown by {@link RegexBuilder} methods
  */
-@SuppressWarnings("WeakerAccess")
 public final class RegexBuilderException extends Exception {
     /**
      * The regex string as it currently stands
@@ -16,7 +15,7 @@ public final class RegexBuilderException extends Exception {
      * @param message       Message describing the error
      * @param stringBuilder The current regex string being built by {@link RegexBuilder}
      */
-    public RegexBuilderException(final String message, final StringBuilder stringBuilder) {
+    RegexBuilderException(final String message, final StringBuilder stringBuilder) {
         super(message);
         regexString = stringBuilder.toString();
     }
